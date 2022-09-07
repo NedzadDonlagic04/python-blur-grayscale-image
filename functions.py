c4d77dfa26701c49f410437acfc3fe7cff416341
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-09-07 20:27:33 +0200

    Added a function to blur and save the image given

--- functions.py
+++ functions.py
@@ -1,1 +1,9 @@
 import cv2 as cv
+from PIL import Image
+
+def blur(img):
+    img = cv.imread(img)
+    blurredImg = cv.blur(img, (15, 15))
+    print(blurredImg)
+    newImage = Image.fromarray(blurredImg)
+    newImage.save('blurredImg.png')
