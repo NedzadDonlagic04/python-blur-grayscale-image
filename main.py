c5e4f9b6511c891b37ad49ba900a4896fc9cf046
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-09-07 20:49:03 +0200

    Added new functions to flags
    Added the grayscale function to the -g flag
    Added the blur and grayscale functions to the
    -a flag

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import functions as func
 def main():
     parser = arg.ArgumentParser(description='Path to an image')
 
-    parser.add_argument('-b', '--blur', help='Blur passed image', default=None)
+    parser.add_argument('-b', '--blur', help='Blur passed image', default='')
     parser.add_argument('-g', '--grayscale', help='Grayscale passed image', default='')
     parser.add_argument('-a', '--all-options', help='Blur and grayscale passed image', default='')
 
@@ -19,10 +19,11 @@ def main():
         func.blur(blurPath)
 
     if os.path.exists(grayScalePath):
-        print('Exists')
+        func.grayscale(grayScalePath)
 
     if os.path.exists(allOptionsPath):
-        print('Exists')
+        func.blur(allOptionsPath)
+        func.grayscale(allOptionsPath)
 
 if __name__ == '__main__':
     main()
